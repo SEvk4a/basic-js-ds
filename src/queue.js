f507d56dafdaa3b3d5ffe1f8608c63968b8a5eea
SEvk4a <SEvk4a@mail.ru>
2022-05-02 18:57:06 +0300

    feat: solve 3/4 tasks

--- src/queue.js
+++ src/queue.js
@@ -1,6 +1,6 @@
 const { NotImplementedError } = require('../extensions/index.js');
 
-// const { ListNode } = require('../extensions/list-node.js');
+const { ListNode } = require('../extensions/list-node.js');
 
 /**
  * Implement the Queue with a given interface via linked list (use ListNode extension above).
@@ -14,23 +14,37 @@ const { NotImplementedError } = require('../extensions/index.js');
  * queue.getUnderlyingList() // returns { value: 3, next: null }
  */
 class Queue {
-
-  getUnderlyingList() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  constructor() {
+    this.head = null;
+    this.length = 0;
   }
 
-  enqueue(/* value */) {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+  getUnderlyingList() {
+    return this.head;
+  }
+
+  enqueue(value) {
+    if (this.length) {
+      let node = this.head;
+      while (node.next) {
+        node = node.next;
+      }
+      node.next = new ListNode(value);
+    } else {
+      this.head = new ListNode(value);
+    }
+    this.length++;
   }
 
   dequeue() {
-    throw new NotImplementedError('Not implemented');
-    // remove line with error and write your code here
+    const node = this.head;
+    this.head = this.head.next;
+    this.length--;
+
+    return node.value;
   }
 }
 
 module.exports = {
-  Queue
+  Queue,
 };
